c4e3ebf1badff681e8c6dc901eab91f61c1bec38
Bouallaga Abdelkader <babdelka@e2r8p7.1337.ma>
2021-12-23 02:01:18 +0100

    update

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 if ! command -v docker &> /dev/null
-then
+then {
     echo "docker could not be found. installing Docker"
     sudo apt-get update
     sudo apt-get install -y curl
@@ -10,17 +10,20 @@ then
     sudo cp ./update.sh /
     sudo printf "\n@reboot /update.sh\n" > /etc/crontab
     run()
-else
+}
+else {
     echo "docker already installed"
     run()
+}
 fi
 
 function run {
     if ! docker info 2>/dev/null
-    then
+    then {
         echo "docker not ready waiting 5s"
         sleep 5
         run()
+    }
     else
         sudo docker-compose up &
     fi
