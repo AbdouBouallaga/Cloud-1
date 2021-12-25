2c9f67825d303435b66da7ca26f9612be50ac26e
Bouallaga Abdelkader <babdelka@e2r5p7.1337.ma>
2021-12-25 23:48:26 +0100

    hide db

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ docker build -t cloud-1_pma:1 ./pma
 docker build -t cloud-1_wordpress:1 ./wp
 docker build -t cloud-1_mysql:1 ./db
 docker network create --subnet=172.180.0.0/16 cloud-1_net
-docker run --net cloud-1_net --ip 172.180.0.2 -d -p 127.0.0.1:3306:3306 --name cloud_mysql cloud-1_mysql:1
+docker run --net cloud-1_net --ip 172.180.0.2 -d --name cloud_mysql cloud-1_mysql:1
 docker run --net cloud-1_net --ip 172.180.0.3 -d -p 80:80 -p 443:443 --name cloud_wordpress cloud-1_wordpress:1
 docker run --net cloud-1_net --ip 172.180.0.4 -d -p 8080:80 --name cloud_phpmyadmin cloud-1_pma:1
